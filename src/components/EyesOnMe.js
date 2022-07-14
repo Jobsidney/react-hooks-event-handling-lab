fbea59fa60e4324fb4bc00fce98bc04bf3d0237f
Jobsidney <jobsidney67@gmail.com>
2022-07-14 14:54:20 +0300

    DOne with assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+function fire(){
+    console.log('Good!');
+}
+function blur(){
+    console.log('Hey! Eyes on me!');
+}
+export default function EyesOnMe() {
+ 
+  return (
+    <div>
+      <button onFocus={fire} onBlur={blur}>Eyes on me</button>
+    </div>
+  )
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,18 @@
 // Code Keypad Component Here
+import React,{useState} from 'react'
+
+
+
+function onAlt(event){
+    event.preventDefault();
+    // console.log(event.target.value);
+    console.log('Entering password...');
+}
+
+export default function Keypad() {
+  return (
+    <div>
+      <input type="password" placeholder='password' onChange={onAlt}/>
+    </div>
+  )
+}
